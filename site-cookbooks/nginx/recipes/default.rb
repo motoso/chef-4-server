93c445a52f844903ff0afdc15b50e25f227cd4d7
motoso <motosonomoto@gmail.com>
2016-02-07 03:38:44 +0900

    nginxが動くようになる．でもファイアウォールは通らない

--- site-cookbooks/nginx/recipes/default.rb
+++ site-cookbooks/nginx/recipes/default.rb
@@ -7,13 +7,33 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# add the EPEL repo
+yum_repository 'epel' do
+  description 'Extra Packages for Enterprise Linux'
+  mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
+  gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
+  action :create
+end
+
+service 'iptables' do
+  action [:disable, :stop]
+end
+
+# @see http://qiita.com/DQNEO/items/e0a6812566e44c7f9577
+template "/etc/yum.repos.d/nginx.repo" do
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
 package "nginx" do
   action :install
+  options "--enablerepo=nginx"
 end
 
 service "nginx" do
   supports :status => true, :restart => true, :reload => true
-  action [ :enable, :start]
+  action [ :enable]
 end
 
 template "nginx.conf" do
@@ -22,5 +42,6 @@ template "nginx.conf" do
   owner "root"
   group "root"
   mode 0644
+  notifies :start, 'service[nginx]'
   notifies :reload, 'service[nginx]'
 end
